31749c83a6c09d394e134cb5e9164091da623b4c
D42rch <gguichard25@gmail.com>
2022-03-07 21:26:04 +0100

    test

---
+++ test_db.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql');
+
+var client = mysql.createConnection({
+    host: '185.221.182.196',
+    user: 'qtgwmxaw_root',
+    password: 'YJV2j.sTAUF.psN',
+    database: 'qtgwmxaw_rr',
+});
+
+client.connect();
